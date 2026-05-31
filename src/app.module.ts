58dbd6457cb8d5a5d2b8818f55a9a57a357c033d
Khalifa Al Hasan <khlfaalhsn5@gmail.com>
2026-06-01 03:26:54 +0700

    chore: register auth and organization modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { OrganizationModule } from './modules/organization/organization.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, OrganizationModule],
   controllers: [AppController],
   providers: [AppService],
 })
